fa29d802cde706f6c4625d63b22c1749f01118ae
bastien-vdb <bastien.deboisrolin@gmail.com>
2022-10-21 10:20:37 +0200

    search activated

--- src/App.js
+++ src/App.js
@@ -3,43 +3,56 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Coins from './components/Coins';
 
+//Just for make console.log faster
 const cs = console.log;
 
 function App() {
 
-  const [datatab, setData] = useState([]);
+  //Creating 2 useState filled after API Call:
 
+  // 1-One to show data (filter or not)
+  const [datatab, setData] = useState([]);
+  // 2-the second to have an unchanged list even after arr.filter()
+  const [listSaved, setListSaved] = useState([]);
+
+  //Call API definition
   const getInfo = async () => {
     const tab = [];
     const res1 =
+    //fixed address with no security key, so we can write it directly here
       await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false')
         .then(res => { return res.json() })
-    // .then(res=>cs(res))
 
     res1.forEach(element => {
       const obj = { id: element.id, image: element.image, symbol: element.symbol, current_price: element.current_price, priceChangePourcent24: element.market_cap_change_percentage_24h, market_cap_rank: element.market_cap_rank }
       setData(tab => [...tab, obj]);
       tab.push(obj)
     })
+    //filling of the list
     setData(() => [...tab]);
-    cs(datatab)
+    setListSaved(()=> [...tab]);
   }
 
+  //using of filter() fnct for the Search field
   const search = (e) => {
-    cs(e.target.value);
+    let tab = [...listSaved];
+    tab = tab.filter(element=>(element.id).includes(e.target.value)||(element.symbol).includes(e.target.value));
+    setData(()=>[...tab]);
   }
 
+  //Call API inserted into an UseEffect
   useEffect(() => {
     getInfo();
   }, [])
 
   return (
     <div>
-      <div className='bg-black h-full flex flex-col items-center justify-center'>
+      <div className='h-full flex flex-col items-center justify-center'>
         <div id='search' className='mt-12'>
-          <input id='crypto' onChange={(e)=>search(e)} className='bg-gray-800 text-center p-2 rounded-md text-4xl' placeholder='Search a coin' />
+          <input id='crypto' onChange={(e)=>search(e)} className='bg-gray-800 text-center p-2 rounded-md mx-4 lg:text-4xl' placeholder='Search a coin' />
         </div>
-        <div id='container' className=' w-2/3 h-fit justify-around rounded-xl flex flex-wrap gap-10 p-10 m-10'>
+        <div id='container' className='w-full m-2 p-6 lg:w-2/3 lg:p-10 lg:m-10 h-fit justify-center rounded-xl flex flex-wrap gap-10'>
+          {/* All the props are passed directly into the component with {...e} */}
           {datatab.map(e => <Coins {...e} />)}
         </div>
       </div>
--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-function Coins({ image, symbol, priceChangePourcent24, current_price, market_cap_rank}) {
+//Destructuring of props
+function Coins({ key, image, symbol, priceChangePourcent24, current_price, market_cap_rank}) {
     return (
         <div>
-            <div id='minibox' className='h-48 w-48 bg-black border-2 border-yellow-600 rounded-xl hover:scale-125 hover:rotate-3 duration-300 cursor-pointer ease-in-out flex flex-col gap-2 p-4 shadow-lg shadow-orange-300'>
+            <div id='minibox' className='h-60 w-36 md:h-48 md:w-48 bg-black border-2 border-yellow-600 rounded-xl hover:scale-125 hover:rotate-3 duration-300 cursor-pointer ease-in-out flex flex-col gap-2 p-4 shadow-lg shadow-orange-300'>
                 <div className='flex justify-evenly w-full'>
                     <img src={image} alt='crypto image' className='h-8 w-8' />
-                    <p className='text-xl uppercase'>{symbol}</p>
+                    <p className='md:text-xl uppercase'>{symbol}</p>
                 </div>
                 <div className='text-center flex flex-col gap-6 font-bold'>
-                    <p className={priceChangePourcent24>=0? 'text-green-500 text-3xl':'text-red-500 text-3xl'}>{Math.floor(priceChangePourcent24)} %</p>
+                    <p className={priceChangePourcent24>=0? 'text-green-500 text-xl md:text-3xl':'text-red-500 text-xl md:text-3xl'}>{Math.floor(priceChangePourcent24)} %</p>
                     <p>Price: <span className='text-sky-400'>{current_price} $</span></p>
                     <p>Marketcap N°: <span className='text-sky-400'>{market_cap_rank}</span></p>
                 </div>
